2d26b6161dde98e71664c9f7580314ed7b517f19
Amul <amul@jetbro.in>
2020-12-23 19:54:30 +0530

    make it more accurate

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-eval $(printenv | awk -F= '{print "export " $1"=""\""$2"\"" }' >>  /etc/profile.d/environmnets.sh)
+eval $(printenv | awk -F= '{print "export " "\""$1"\"""=""\""$2"\"" }' >> /etc/profile.d/environmnets.sh)
 cron -f -l 8 & 
 apache2-foreground
